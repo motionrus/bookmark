a3dea4adcb0e795d35b7d625f8c70c4a0023f230
Ruslan Tyutin <tytin@narod.ru>
2017-12-14 15:42:23 +0300

    правки

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Welcome to bookmarks project. It work on django.
+
+You need add file bookmarks/settings_local.py
+
+example file:
+
+```
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'Cryptographic signing'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+```
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
