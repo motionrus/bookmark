4118fdbdd0ba258f6cfeb5d2cea59f987e908b9b
ivan <skich1984@gmail.com>
2017-12-19 07:50:14 +0300

    password reset/password reset in prod

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -7,7 +7,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('links/', include('main.urls')),
-	path('signup/', accounts_views.signup, name='signup'),
-	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-	path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', include('accounts.urls')),
 ]
