2e14a9f05f37023a61ee5e84a6db84aa42b80764
rus-vista <tytin@narod.ru>
2018-01-03 17:40:18 +0300

    realize tags for protocol og:

--- main/views.py
+++ main/views.py
@@ -30,20 +30,41 @@ def get_url(request, number_links):
     format_link = '<h1>{}</h1><p>{}</p>'.format(link.title, link.text)
     return HttpResponse(format_link)
 
+
 def add_link(request):
     form = LinkBookMark(request.POST)
     if form.is_valid():
         url = (form.cleaned_data['url'])
         r = requests.get(url)
-        site = BeautifulSoup(r.text, 'html.parser')
-        title = site.title.text
-        text = site.p.text
-        short_text =  site.p.text[:20]
-        print('timezone={}, title={}, text={}, short_text={}, '.format(timezone.now(), title, text, short_text))
-        BookMark(pub_date=timezone.now(), url=url, title=title, text=text, short_text=short_text).save()
-
-
+        preview = get_meta_link(url)
+        preview['url'] = url
+        if not preview['title']:
+            site = BeautifulSoup(r.text, 'html.parser')
+            preview['title'] = site.title.text
+        print('timezone={}, title={title}, image={image}, descr={description}, '.format(timezone.now(), **preview))
+        BookMark(pub_date=timezone.now(), **preview).save()
     return form
+
+
+def get_meta_link(url):
+    """return dictionary meta tags"""
+    headers = ''
+    if 'vk.com' in url:
+        # vk.com url is very strange, because without headers is not works
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'
+        }
+    dict_meta = {x: '' for x in ['title', 'image', 'description']}
+    r = requests.get(url=url, headers=headers)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        print(soup.find('meta', property='og:'))
+        try:
+            for key in dict_meta:
+                dict_meta[key] = soup.find('meta', property='og:'+key)['content']
+        except TypeError:
+            pass
+    return dict_meta
 
 def save_in_db(request):
     pass
