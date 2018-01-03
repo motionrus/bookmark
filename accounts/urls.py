7f5969da166021283e1e1e6788a048a4045eb7a3
rus-vista <tytin@narod.ru>
2018-01-03 12:32:07 +0300

    my

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from accounts import views as accounts_views
 from django.contrib.auth import views as auth_views
-
-
 
 
 urlpatterns = [
@@ -11,7 +9,7 @@ urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 	path('password_reset/', auth_views.password_reset, name='password_reset'),
     path('password_reset/done', auth_views.password_reset_done, name='password_reset_done'),
-    path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    re_path(r'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
 	path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
