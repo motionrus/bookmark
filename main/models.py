30535e47b7c3b7899dfdbb31ab1f1abd4c15e5e6
rus-vista <tytin@narod.ru>
2018-01-08 10:44:20 +0300

    add og:site_name and add to template tag site_name. Change views.py func by recommendated vakimov

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class BookMark(models.Model):
     pub_date = models.DateTimeField('date saved')
     url = models.URLField(max_length=200)
+    site_name = models.TextField(max_length=200, default='')
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=500, default='None')
     image = models.URLField(max_length=200, default='')
--- main/views.py
+++ main/views.py
@@ -10,8 +10,10 @@ from django.urls import reverse
 from django.views.generic import ListView
 # Create your views here.
 
+
 def last_five_links(index):
     return BookMark.objects.order_by('-pub_date')[index*5:index*5+5]
+
 
 def index(request, number_links=0):
     """return last 5 links"""
@@ -19,7 +21,7 @@ def index(request, number_links=0):
         if 'url' in request.POST:
             parse_link(request)
         if 'delete_pk_id' in request.POST:
-            delete_post(request)
+            delete_post(request.POST['delete_pk_id'])
     form = LinkBookMark()
     link = last_five_links(number_links)
 
@@ -45,42 +47,48 @@ def parse_link(request):
     form = LinkBookMark(request.POST)
     if form.is_valid():
         url = form.cleaned_data['url']
-        r = requests.get(url)
-        preview = get_meta_link(url)
+        html_preview = get_html(url)
+        preview = get_meta_tags(html_preview)
         preview['url'] = url
-        if not preview['title']:
-            site = BeautifulSoup(r.text, 'html.parser')
-            preview['title'] = site.title.text
-        print('timezone={}, title={title}, image={image}, descr={description}, '.format(timezone.now(), **preview))
+        # logging
+        print('\ttimezone={}'.format(timezone.now()))
+        for key in preview:
+            print('\t{}={}'.format(key, preview[key]))
         BookMark(pub_date=timezone.now(), **preview).save()
     return form
 
 
-def get_meta_link(url):
+def get_meta_tags(html):
     """return dictionary meta tags"""
+    dict_meta = {x: '' for x in ['title', 'image', 'description', 'site_name']}
+    soup = BeautifulSoup(html, 'html.parser')
+    for key in dict_meta:
+        soup_find = soup.find('meta', property='og:'+key)
+        if soup_find:
+            dict_meta[key] = soup_find['content']
+            continue
+        soup_find = soup.find('meta', attrs={'name': key})
+        if soup_find:
+            dict_meta[key] = soup_find['content']
+    return dict_meta
+
+
+def get_html(url):
     headers = ''
     if 'vk.com' in url:
         # vk.com url is very strange, because without headers is not works
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'
         }
-    dict_meta = {x: '' for x in ['title', 'image', 'description']}
     r = requests.get(url=url, headers=headers)
     if r.status_code == 200:
-        soup = BeautifulSoup(r.text, 'html.parser')
-        print(soup.find('meta', property='og:'))
-        try:
-            for key in dict_meta:
-                dict_meta[key] = soup.find('meta', property='og:'+key)['content']
-        except TypeError:
-            pass
-    return dict_meta
+        return r.text
+
 
 def save_in_db(request):
     pass
 
 
-def delete_post(request):
-    pk_id = request.POST['delete_pk_id']
-    BookMark.objects.get(pk=int(pk_id)).delete()
+def delete_post(pk_id):
+    BookMark.objects.get(pk=pk_id).delete()
     return True
