973e19121713da3c17178822e4186250672c88ac
Ruslan Tyutin <tytin@narod.ru>
2018-01-10 18:52:27 +0300

    intermediate commit

--- main/forms.py
+++ main/forms.py
@@ -4,3 +4,6 @@ from django import forms
 class LinkBookMark(forms.Form):
     url = forms.URLField(label='Add Link', max_length=255)
 
+
+class MyTest(forms.Form):
+    url = forms.URLField(label='Add Link', max_length=255)
