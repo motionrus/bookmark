dfed720d2d3e9d91e183ef42f90fb30179612b28
Ruslan Tyutin <tytin@narod.ru>
2018-01-11 10:55:12 +0300

    first template ended

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,10 @@
 from django import forms
 
 
-class LinkBookMark2(forms.Form):
-    url = forms.URLField(label='Add Link', max_length=255)
-
-
 class LinkBookMark(forms.Form):
-    attrs = {
-        'id' : "inlineFormInputName2",
-        'placeholder' : "URL",
+    attr = {
+        'id': "inlineFormInputName2",
+        'placeholder': "URL",
         'class': 'form-control is-valid'
     }
-    url = forms.URLField(label='Add Link', max_length=255, widget=forms.TextInput(attrs=attrs))
+    url = forms.URLField(label='Add Link', max_length=255, widget=forms.TextInput(attrs=attr))
