81326ce5beac9f27d77e17f5d007064f3d28792a
Ruslan Tyutin <tytin@narod.ru>
2018-01-11 12:12:17 +0300

    create test model

--- main/models.py
+++ main/models.py
@@ -14,3 +14,14 @@ class BookMark(models.Model):
 
     def __str__(self):
         return self.title
+
+class BookMarkForTest(models.Model):
+    pub_date = models.DateTimeField('date saved')
+    url = models.URLField(max_length=200)
+    site_name = models.TextField(max_length=200, default='')
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500, default='None')
+    image = models.URLField(max_length=200, default='')
+
+    def __str__(self):
+        return self.title
