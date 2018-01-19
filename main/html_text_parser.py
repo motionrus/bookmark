1cd550c76c9a24a9b6ccae4105b396d5710dba76
Kirill Stepanov <stepanovkd@yandex.ru>
2018-01-19 07:51:50 +0300

    Added html-word  parser for search

---
+++ main/html_text_parser.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import re
+import urllib.request
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+# url_ = 'http://www.nytimes.com/2009/12/21/us/21storm.html'
+# url_ = 'http://medgadgets.ru/shop/katalog/gadgety-dlia-doma.html'
+
+
+def tag_visible(element):
+    html_tags = ['style', 'script', 'head', 'title', 'meta', '[document]']
+    if element.parent.name in html_tags:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.lower().strip() for t in visible_texts)
+
+
+def improve_tokenized_text(_tokens):
+    if _tokens is not None:
+        sr_en = stopwords.words('english')
+        sr_ru = stopwords.words('russian')
+        clean_tokens = []
+        for token in _tokens:
+            # Cleaning non-alphabetic characters
+            token_cleaned = re.sub('[^A-Za-zа-яА-Я]', '', token)
+
+            if len(token_cleaned) <= 2:
+                pass
+            elif token_cleaned in sr_en:
+                pass
+            elif token_cleaned in sr_ru:
+                pass
+            else:
+                clean_tokens.append(token_cleaned)
+
+        return clean_tokens
+    else:
+        return None
+
+
+def tokenize(_text):
+    word_hash_map = {}
+    sent_text = nltk.sent_tokenize(_text)
+    for sentence in sent_text:
+        tokenized_text = nltk.word_tokenize(sentence)
+        tokenized_text = improve_tokenized_text(tokenized_text)
+        tagged = nltk.pos_tag(tokenized_text)
+        for word, tag in tagged:
+            if tag.startswith('NN') or tag.startswith('VB') or tag.startswith('JJ'):
+                if word in word_hash_map:
+                    word_hash_map[word] = word_hash_map[word] + 1
+                else:
+                    word_hash_map[word] = 1
+    return word_hash_map
+
+
+def get_url_word_analytics(_url):
+
+    html = urllib.request.urlopen(_url).read()
+    html_text = text_from_html(html)
+    word_dict = tokenize(html_text)
+    return word_dict
--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from main.models import BookMark
+from main.models import BookMark, Word_Analytics
+from main.html_text_parser import get_url_word_analytics
 from .forms import LinkBookMark
 from django.template import loader
 from django.utils import timezone
 import requests
 from bs4 import BeautifulSoup
 from django.core.paginator import Paginator
-from django.urls import reverse
-from django.views.generic import ListView
+# from django.urls import reverse
+# from django.views.generic import ListView
 from django.contrib.auth.models import User
+
 # Create your views here.
 
 DEFAULT_PAGE_SIZE = 6
@@ -23,6 +25,7 @@ def index(request, number_links=1, size=DEFAULT_PAGE_SIZE):
         if request.user.is_authenticated is True:
             if 'url' in request.POST:
                 form = parse_link(request)
+                save_url_word_analytics(request)
             elif 'delete_pk_id' in request.POST:
                 delete_post(request.POST['delete_pk_id'])
         else:
@@ -44,6 +47,33 @@ def get_url(request, number_links):
     return index(request, number_links)
 
 
+def get_search_results(request):
+    template = loader.get_template('search_results.html')
+    page_output = Paginator(
+        BookMark.objects.order_by('-pub_date'),
+        DEFAULT_PAGE_SIZE).page(1)
+
+    context = {
+        'search_results': page_output.object_list,
+        'page_output': page_output,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def save_url_word_analytics(request):
+    # Url text analyzer
+    user_bookmarks = BookMark.objects.filter(user=request.user)
+    # last_bookmark_date = user_bookmarks.aggregate(Max('pub_date'))
+    # l = BookMark.objects.get(user=request.user, pub_date=last_bookmark_date)
+    last_bookmark = user_bookmarks[len(user_bookmarks) - 1]
+    url_word_map = get_url_word_analytics(last_bookmark.url)
+
+    for key in url_word_map.keys():
+        new_word_analytics = Word_Analytics(word=key, frequency=url_word_map.get(key, 0))
+        new_word_analytics.bookmark_id = last_bookmark.id
+        new_word_analytics.save()
+
+
 def parse_link(request):
     form = LinkBookMark(request.POST)
     if form.is_valid():
@@ -53,8 +83,8 @@ def parse_link(request):
         preview['url'] = url
         current_user = User.objects.get(username=request.user)
         preview['user'] = current_user
-        for key in preview:
-            print('\t{}={}'.format(key, preview[key]))
+        # for key in preview:
+        #     print('\t{}={}'.format(key, preview[key]))
         BookMark(pub_date=timezone.now(), **preview).save()
 
     return form
@@ -65,7 +95,7 @@ def get_meta_tags(html):
     dict_meta = {x: '' for x in ['title', 'image', 'description', 'site_name']}
     soup = BeautifulSoup(html, 'html.parser')
     for key in dict_meta:
-        soup_find = soup.find('meta', property='og:'+key)
+        soup_find = soup.find('meta', property='og:' + key)
         if soup_find:
             dict_meta[key] = soup_find['content']
             continue
